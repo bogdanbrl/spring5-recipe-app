d8b388c317d3911940d03abf7f8f2be2a4a8d0d4
Bogdan Brl <bogdan.brlx@gmail.com>
2021-04-29 18:35:19 +0300

    add repositories

--- src/main/java/com/bogdanbrl/recipe/controllers/IndexController.java
+++ src/main/java/com/bogdanbrl/recipe/controllers/IndexController.java
@@ -1,13 +1,36 @@
 package com.bogdanbrl.recipe.controllers;
 
+import com.bogdanbrl.recipe.domain.Category;
+import com.bogdanbrl.recipe.domain.UnitOfMeasure;
+import com.bogdanbrl.recipe.repositories.CategoryRepository;
+import com.bogdanbrl.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category id is: " + categoryOptional.get().getId());
+        System.out.println("Unit of measure id is: " + optionalUnitOfMeasure.get().getId());
+
         return "index";
     }
+
+
 }
